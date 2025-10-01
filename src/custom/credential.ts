d626b6691d452c87275f8b54df59537c63bf3bc3
SSbit01 <ss42701@outlook.com>
2025-10-01 12:03:53 +0200

    docs

--- src/custom/credential.ts
+++ src/custom/credential.ts
@@ -5,9 +5,10 @@ import { ERR_CREDENTIAL_INVALID } from "@/lib/errors.js"
 
 /**
  * Verify the initial credential/ID before generating and sending an OTP.
- * The request header should have `"Content-Type": "application/json"`.
- * Customize this validator as you like.
- * The validation target must be `json` and return a credential/ID (not shared with the client) of type `string` or `number` to later confirm that the credentials have been verified.
+ * - The request header should have `"Content-Type": "application/json"`.
+ * - Customize this validator as you like.
+ * - The validation target must be `json` and return a credential/ID (not shared with the client) of type `string` or `number` to later confirm that the credentials have been verified.
+ * - Remember a JSON can be an object/array or a value (string, number, boolean, null).
  */
 const credentialValidator = validator("json", (body, c) => {
 
