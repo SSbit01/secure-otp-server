f93308c25188dcdfa7421c0ad76f6d602f66c2c8
Stefan Samson <ss42701@outlook.com>
2026-01-27 19:00:18 +0100

    fix

--- src/lib/kms.js
+++ src/lib/kms.js
@@ -70,7 +70,6 @@ export async function rotateKek(c, kekId) {
 
   if (!currentKekId || kekId === currentKekId) {
     console.warn("A KEK rotation has been triggered.")
-    deleteOtpCookie(c)
     await storeKek(c, await createKek(), createRandomIdString(KEK_ID_BYTES))
     console.log("KEK rotation completed.")
   }
